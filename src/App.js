3b2108511c9a1ba28cb9be22e1c25f4d98fe5dd2
DigitalCrafts <digitalcrafts@DigitalCraftss-MacBook-Air-2.local>
2019-02-15 14:01:41 -0500

    React App

--- src/App.js
+++ src/App.js
@@ -1,28 +1,98 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Poster from './poster';
+import NavBar from './NavBar';
+
 
 class App extends Component {
+  // in order to use "this" we have to have constructor
+  constructor(){
+    super();
+    this.state = {
+      movieList: []
+	}
+	this.movieSearch = this.movieSearch.bind(this);
+  }
+
+  componentDidMount(){
+	const url = "https://api.themoviedb.org/3/movie/now_playing?api_key=fec8b5ab27b292a68294261bb21b04a5";
+	
+	// fetch is a replacement for $.getJSON/$.ajax/axios
+	fetch(url)
+	.then((response)=>{
+	  return response.json();
+	})
+	.then((myJson)=>{
+		const results = myJson.results;
+		console.log(results)
+		// this.state.moviesToShow = results // BAD BAD BAD
+							 //    ^
+							 //    |
+							 //   BAD!!!!!
+		this.setState({
+			movieList: results
+		});
+	});
+
+	console.log("Checking... yes! It's mounted");
+
+}
+
+	movieSearch(e){
+		console.log(e)
+		e.preventDefault();
+		console.log("Form submitted!");
+		const movieTitle = document.getElementById('searchTerm').value;
+		const url = 'https://api.themoviedb.org/3/search/movie?api_key=fec8b5ab27b292a68294261bb21b04a5&query='+movieTitle;
+		fetch(url)
+		.then((response)=>{
+		  return response.json();
+		})
+		.then((myJson)=>{
+			const results = myJson.results;
+			console.log(results)
+			this.setState({
+				movieList: results
+			});
+		});		
+	}
+  
   render() {
+	  const posters = this.state.movieList.map((movie,i)=>{
+		   return(<Poster key={i} movie={movie} />)
+		//    movie  = this.state.movieList[i]
+	  })
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    	<div className="container">
+			<NavBar />
+			<div className="row">
+				{/* <h1>The movie app...again.</h1> */}
+
+				<form onSubmit={this.movieSearch}>
+					<input id="searchTerm" type="text" placeholder="Movie Title" />
+					<button type="submit" className="btn waves-effect btn-light">Search</button>
+				</form>
+
+				{posters}
+			</div>
+      	</div>
     );
   }
 }
 
 export default App;
+
+// inside of React...
+// if(this.Component.props.onSubmit){
+// 	// onSubmit handler passed
+// 	// call the function, that was passed
+// 	this.Component.props.onSubmit()
+// }
+
+// for(let i=0; i<5; i++){
+// 	for(let j=0; j<5; j++){
+// 		if(j==3){
+// 			break
+// 		}
+// 	}
+// }
